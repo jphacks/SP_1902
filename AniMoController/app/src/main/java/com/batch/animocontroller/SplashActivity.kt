8916e20075067d03198164431f554779ecce3a65
Takumi Fujimoto <mottypublic@gmail.com>
2019-10-27 03:32:50 +0900

    splashにビデオ追加とタイトルバー消去

--- AniMoController/app/src/main/java/com/batch/animocontroller/SplashActivity.kt
+++ AniMoController/app/src/main/java/com/batch/animocontroller/SplashActivity.kt
@@ -1,19 +1,37 @@
 package com.batch.animocontroller
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
+
+    private val SPLASHDELAY: Long = 0
+    private var mDelayHandler: Handler? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-    }
 
-    override fun onStart() {
-        super.onStart()
-        val intent = Intent(this, ConnectActivity::class.java)
-        startActivity(intent)
+        val mRunnable: Runnable = Runnable {
+            val intent = Intent(this, ConnectActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        mDelayHandler = Handler()
+        Handler(mainLooper).postDelayed({
+            var moviePath = Uri.parse("android.resource://" + packageName + "/" + R.raw.animo_splash)
+            videoView.setVideoURI(moviePath)
+            videoView.setOnPreparedListener{
+                videoView.start()
+            }
+            videoView.setOnCompletionListener {
+                mDelayHandler!!.postDelayed(mRunnable, SPLASHDELAY)
+            }
+        }, 0)
     }
 }
